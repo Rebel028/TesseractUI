a6d791b560e5ac56e6c356e6a4cdbfa1e7c7d375
Rebel028 <a.e.lazovsky@gmail.com>
2020-07-20 18:05:40 +0300

    rename

--- TesseractUI/MainWindow.xaml.cs
+++ TesseractUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TessaractUI
+namespace TesseractUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
